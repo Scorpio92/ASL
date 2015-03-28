fcba6ef3e4ab8bb189b6c7a21424d8b941538acc
scorpio92 <scorpio92@mail.ru>
2015-03-28 20:52:27 +1000

    ASL v1.1: add interface for full list hash sums of the /system partition. File list in /proc/asl/. Delete: root_hash and files_count. Now it is usless. Main aim - checking status for each file.

--- kernel/security/asl/asl.c
+++ kernel/security/asl/asl.c
@@ -2,6 +2,7 @@
 #include <asm/uaccess.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <linux/module.h>
 
 #define ASL_ENTRY "asl"
 #ifdef CONFIG_ASL_DISABLED
@@ -9,23 +10,30 @@
 #else
 #define ENABLED_FLAG "1"
 #endif
-#define ASL_version "1.0"
+#define ASL_version "1.1"
 #define MAX_PROC_SIZE 4 //размер буфера для записи статуса
+//#define MAX_PROC_LIST_SIZE 100 //размер буфера для записи
 
+static char list[] = {
+#include "list"
+};
+/*
 static char files_count[] = {
 #include "files_count"
 };
 static char root_hash[] = {
 #include "root_hash"
 };
+*/
 static char proc_data[MAX_PROC_SIZE];
 
 static struct proc_dir_entry *asl_dir;
 static struct proc_dir_entry *enabled_file; //вкл/откл механизма
 static struct proc_dir_entry *status_file; //переменная для статуса
 static struct proc_dir_entry *version_file; //переменная для версии
-static struct proc_dir_entry *root_hash_file; //переменная для корневой хеш суммы
-static struct proc_dir_entry *files_count_file; //переменная для количества файлов
+static struct proc_dir_entry *list_file; //переменная для списка хеш сумм
+//static struct proc_dir_entry *root_hash_file; //переменная для корневой хеш суммы
+//static struct proc_dir_entry *files_count_file; //переменная для количества файлов
 
 int read_enabled_flag(char *buf,char **start,off_t offset,int count,int *eof,void *data )
 {
@@ -33,7 +41,7 @@ int len=0;
 len = sprintf(buf,ENABLED_FLAG);
 return len;
 }
-
+/*
 int read_files_count(char *buf,char **start,off_t offset,int count,int *eof,void *data )
 {
 int len=0;
@@ -47,6 +55,7 @@ int len=0;
 len = sprintf(buf,root_hash);
 return len;
 }
+*/
 
 int read_ver(char *buf,char **start,off_t offset,int count,int *eof,void *data )
 {
@@ -72,7 +81,64 @@ if(copy_from_user(proc_data, buf, count))
 
 return count;
 }
+//********************list start******************************************
+static void *my_seq_start(struct seq_file *s, loff_t *pos)
+{
+	static unsigned long counter = 0;
 
+	/* beginning a new sequence ? */	
+	if ( *pos == 0 )
+	{	
+		/* yes => return a non null value to begin the sequence */
+		return &counter;
+	}
+	else
+	{
+		/* no => it's the end of the sequence, return end to stop reading */
+		*pos = 0;
+		return NULL;
+	}
+}
+
+static void *my_seq_next(struct seq_file *s, void *v, loff_t *pos)
+{
+	unsigned long *tmp_v = (unsigned long *)v;
+	(*tmp_v)++;
+	(*pos)++;
+	return NULL;
+}
+
+static void my_seq_stop(struct seq_file *s, void *v)
+{
+	/* nothing to do, we use a static value in start() */
+}
+
+static int my_seq_show(struct seq_file *s, void *v)
+{
+	seq_printf(s, list);
+        return 0;
+}
+
+static struct seq_operations my_seq_ops = {
+	.start = my_seq_start,
+	.next  = my_seq_next,
+	.stop  = my_seq_stop,
+	.show  = my_seq_show
+};
+
+static int my_open(struct inode *inode, struct file *file)
+{
+	return seq_open(file, &my_seq_ops);
+};
+
+static struct file_operations my_file_ops = {
+	.owner   = THIS_MODULE,
+	.open    = my_open,
+	.read    = seq_read,
+	.llseek  = seq_lseek,
+	.release = seq_release
+};
+//*****************************************list end****************************
 
 void create_new_proc_entry()
 {
@@ -82,8 +148,14 @@ if(!asl_dir){
     return -ENOMEM;
 }
 enabled_file = create_proc_read_entry("enabled", 0444, asl_dir, read_enabled_flag, NULL);
-files_count_file = create_proc_read_entry("files_count", 0444, asl_dir, read_files_count, NULL);
-root_hash_file = create_proc_read_entry("root_hash", 0444, asl_dir, read_root_hash, NULL);
+//files_count_file = create_proc_read_entry("files_count", 0444, asl_dir, read_files_count, NULL);
+//root_hash_file = create_proc_read_entry("root_hash", 0444, asl_dir, read_root_hash, NULL);
+//****************************************list start*****************************
+list_file = create_proc_entry("list", 0444, asl_dir);
+if (list_file) {
+    list_file->proc_fops = &my_file_ops;
+}
+//****************************************list end*******************************
 version_file = create_proc_read_entry("version", 0444, asl_dir, read_ver, NULL);
 status_file = create_proc_entry("status", 0666, asl_dir);
 if(!status_file){
