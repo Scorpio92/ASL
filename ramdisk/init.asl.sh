793da161fb3aa1bbf114a1fe25d38b14a121cd7f
scorpio92 <scorpio92@mail.ru>
2015-03-29 11:35:47 +1000

    RAMDISK: change check script for new method

--- ramdisk/init.asl.sh
+++ ramdisk/init.asl.sh
@@ -2,50 +2,52 @@
 
 ENABLED=$(cat /proc/asl/enabled)
 
-ORIG_COUNT=$(cat /proc/asl/files_count)
+sha1check() 
+{
+touch /dev/asl/asl_protocol
+
+echo "$(sha1sum -c '/dev/asl/asl_list')" > /dev/asl/asl_protocol
+}
+
+proto_parse()
+{
+touch /dev/asl/detected
+
+cat /dev/asl/asl_protocol | grep "FAILED" > /dev/asl/detected
+}
+
+recovery_mode()
+{
+reboot recovery
+}
+
+check_status()
+{
+cat /dev/asl/detected | 
+(while read line
+do
+if [ -n "$line" ]
+ then
+  echo '0' > /proc/asl/status
+  recovery_mode
+ else
+  echo '1' > /proc/asl/status
+fi
+done
+)
+}
 
 if [ "$ENABLED" == "1" ]
  then
 
-    ORIG_COUNT=$(cat /proc/asl/files_count)
+       cp /proc/asl/list /dev/asl/asl_list
+       
+       sha1check
 
-    CALC_COUNT_FILES=$(find /system -type f | wc -l)
+       proto_parse
 
-    CALC_COUNT_SYMLINKS=$(find /system -type l | wc -l)
+       check_status
 
-    CALC_COUNT=$(($CALC_COUNT_FILES+$CALC_COUNT_SYMLINKS))
-
-    if [ "$ORIG_COUNT" == "$CALC_COUNT" ]
-     then
-
-       touch /dev/asl/asl_list
-
-       touch /dev/asl/root_hash
-
-       for FILE in `find /system -type f -o -type l | sort -f`
-       do
-       SUM_STRING="$SUM_STRING\n$(sha1sum $FILE)"
-       done
-
-       echo -e $SUM_STRING | sed '1d' > /dev/asl/asl_list
-
-       sha1sum '/dev/asl/asl_list' > /dev/asl/root_hash
-
-       ORIG_SUM=$(cat /proc/asl/root_hash)
-
-       CALC_SUM=$(cat /dev/asl/root_hash | cut -b 1-40)
-
-       if [ "$CALC_SUM" == "$ORIG_SUM" ]
-        then
-          echo '1' > /proc/asl/status
-        else
-          echo '0' > /proc/asl/status
-          reboot recovery
-       fi
-    else
-       echo '0' > /proc/asl/status
-       reboot recovery
-    fi
  else
    echo '0' > /proc/asl/status
 fi
