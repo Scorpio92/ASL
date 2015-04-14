a30be17a51da536ea108d6267d0f0c96c6a13370
scorpio92 <scorpio92@mail.ru>
2015-04-15 09:34:22 +1000

    add interface for output hash of the recovery archive (/proc/asl/archive_hash) files_count - now it again need. archive_hash and files_count as example.

--- kernel/security/asl/asl.c
+++ kernel/security/asl/asl.c
@@ -17,14 +17,15 @@
 static char list[] = {
 #include "list"
 };
-/*
+
 static char files_count[] = {
 #include "files_count"
 };
-static char root_hash[] = {
-#include "root_hash"
+
+static char archive_hash[] = {
+#include "archive_hash"
 };
-*/
+
 static char proc_data[MAX_PROC_SIZE];
 
 static struct proc_dir_entry *asl_dir;
@@ -32,8 +33,8 @@ static struct proc_dir_entry *enabled_file; //вкл/откл механизма
 static struct proc_dir_entry *status_file; //переменная для статуса
 static struct proc_dir_entry *version_file; //переменная для версии
 static struct proc_dir_entry *list_file; //переменная для списка хеш сумм
-//static struct proc_dir_entry *root_hash_file; //переменная для корневой хеш суммы
-//static struct proc_dir_entry *files_count_file; //переменная для количества файлов
+static struct proc_dir_entry *archive_hash_file; //переменная для хеш суммы архива восстановления
+static struct proc_dir_entry *files_count_file; //переменная для количества файлов
 
 int read_enabled_flag(char *buf,char **start,off_t offset,int count,int *eof,void *data )
 {
@@ -41,7 +42,7 @@ int len=0;
 len = sprintf(buf,ENABLED_FLAG);
 return len;
 }
-/*
+
 int read_files_count(char *buf,char **start,off_t offset,int count,int *eof,void *data )
 {
 int len=0;
@@ -49,13 +50,12 @@ len = sprintf(buf,files_count);
 return len;
 }
 
-int read_root_hash(char *buf,char **start,off_t offset,int count,int *eof,void *data )
+int read_archive_hash(char *buf,char **start,off_t offset,int count,int *eof,void *data )
 {
 int len=0;
-len = sprintf(buf,root_hash);
+len = sprintf(buf,archive_hash);
 return len;
 }
-*/
 
 int read_ver(char *buf,char **start,off_t offset,int count,int *eof,void *data )
 {
@@ -148,8 +148,8 @@ if(!asl_dir){
     return -ENOMEM;
 }
 enabled_file = create_proc_read_entry("enabled", 0444, asl_dir, read_enabled_flag, NULL);
-//files_count_file = create_proc_read_entry("files_count", 0444, asl_dir, read_files_count, NULL);
-//root_hash_file = create_proc_read_entry("root_hash", 0444, asl_dir, read_root_hash, NULL);
+files_count_file = create_proc_read_entry("files_count", 0444, asl_dir, read_files_count, NULL);
+archive_hash_file = create_proc_read_entry("archive_hash", 0444, asl_dir, read_archive_hash, NULL);
 //****************************************list start*****************************
 list_file = create_proc_entry("list", 0444, asl_dir);
 if (list_file) {
