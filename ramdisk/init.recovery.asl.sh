c74b7b432d9273d89414e1f3ca45106baafe7d78
scorpio92 <scorpio92@mail.ru>
2015-04-24 22:51:05 +1000

    add support for mounting SD-card in recovery mod

--- ramdisk/init.recovery.asl.sh
+++ ramdisk/init.recovery.asl.sh
@@ -1,7 +1,58 @@
 #!/sbin/sh
 
 NEED_RECOVERY=$(cat /dev/asl/need_recovery)
+
 ARCHIVE="/data/system.zip"
+
+power_off()
+{
+umount /mnt/tmpsd
+repoot -p
+}
+
+mount_sd()
+{
+       #how detect block device for sdcard
+       #1) adb shell mount, look 179:<number>
+       #2) cat /proc/partitions, look string </dev/block> with <number>
+
+       P1=$(cat /sdcard.conf | awk -F " " '{print $1}')
+
+       P2=$(cat /sdcard.conf | awk -F " " '{print $2}')
+
+       mkdir /mnt/tmpsd
+
+       mount -r -t $P1 $P2 /mnt/tmpsd
+
+       #change path to the archive
+       ARCHIVE="/mnt/tmpsd/system.zip"
+
+       if [ -f $ARCHIVE ]
+       then
+
+       ORIG_SUM=$(cat /proc/asl/archive_hash)
+
+       CALC_SUM=$(sha1sum $ARCHIVE | awk -F "  " '{print $1}')
+
+       if [ $ORIG_SUM == $CALC_SUM ]
+       then
+
+       unzip_archive
+
+       check_mod_files
+
+       check_doa_files
+
+       else
+       power_off
+       fi
+
+       else
+       power_off
+       fi
+
+       umount /mnt/tmpsd
+}
 
 check_archive()
 {
@@ -14,7 +65,11 @@ then
 unzip_archive
 check_mod_files
 check_doa_files
+else
+mount_sd
 fi
+else
+mount_sd
 fi
 }
 
