6e0f5af91cb323c8fe5d707d6e3c6f2288a7a837
scorpio92 <scorpio92@mail.ru>
2015-05-21 13:37:33 +1000

    init.rc sample (for CyanogenMod 10)

--- device/brand/model/product.mk
+++ device/brand/model/product.mk
@@ -2,3 +2,7 @@
 LOCAL_PATH := device/brand/model
 
 $(call inherit-product-if-exists, $(LOCAL_PATH)/asl.mk)
+
+#ramdisk files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/asl/ramdisk/init.rc:root/init.rc
